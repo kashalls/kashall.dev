deededd4b421c17db574bfafef43bfb46220c5b5
Jordan Jones <jordpjones@gmail.com>
2022-08-16 21:02:54 -0700

    Update fontawesome to 6.12

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,6 +1,6 @@
 /* Import all the font awesome goodness */
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas, faMoon } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faDiscord, faSteam, faGithub, faInstagram, faReddit, faTwitch, faSpotify } from '@fortawesome/free-brands-svg-icons'
-library.add(fas, faTwitter, faDiscord, faSteam, faGithub, faInstagram, faReddit, faTwitch, faSpotify)
+library.add(fas, faTwitter, faDiscord, faSteam, faGithub, faInstagram, faReddit, faTwitch, faSpotify, faMoon)
 dom.watch()
