7fceca5a58f578840a96b8972a0f55ab4da07a08
Kashalls <kashalls@pm.me>
2022-10-28 15:43:31 -0700

    Basic display

--- pages/eternity.vue
+++ pages/eternity.vue
@@ -1,5 +1,18 @@
 <template>
-  <section class="section is-align-content-center" />
+  <section class="section is-align-content-center">
+    <div class="columns is-centered">
+      <div class="column has-text-centered is-half">
+        <h1 class="title is-1 has-text-white">
+          Kashall
+        </h1>
+        <div class="block">
+          <h2 class="title is-3 has-text-link">
+            <span class="has-text-info">ᛃ {{ getBalance.toLocaleString('en-US') }}</span> / <span class="has-text-success">ᛃ {{ totalEther.toLocaleString('en-US') }}</span>
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -9,55 +22,67 @@ export default {
   data () {
     return {
       user: null,
+      totalEther: 0,
       socket: null,
-      timer: null
+      socketHeartbeat: null,
+      etherTimer: null
+    }
+  },
+  computed: {
+    getBalance () {
+      return this.user?.balance || 0
     }
   },
   mounted () {
     const supportsWebSocket = 'WebSocket' in window || 'MozWebSocket' in window
-    if (!supportsWebSocket) { throw new Error('Browser doesn\'t support Websocket connections.') }
-
+    if (!supportsWebSocket) {
+      throw new Error("Browser doesn't support Websocket connections.")
+    }
     this.socket = new WebSocket('wss://api.kashall.dev/socket')
-
     this.socket.addEventListener('open', () => {
-      this.socket.send(
-        JSON.stringify({
-          op: 2,
-          d: {
-            subscribe_to_uuids: [uuid]
-          }
-        })
-      )
-
-      setInterval(() => {
-        this.socket.send(
-          JSON.stringify({
-            op: 3
-          })
-        )
-      }, 30000)
+      this.socket.send(JSON.stringify({
+        op: 2,
+        d: {
+          subscribe_to_uuids: [uuid]
+        }
+      }))
     })
-
     this.socket.addEventListener('message', ({ data }) => {
       data = JSON.parse(data)
-
       if (data.op === 1) {
-        if (this.timer) { return }
-        this.timer = setInterval(() => {
-          this.socket.send(
-            JSON.stringify({
-              op: 3
-            })
-          )
+        if (this.socketHeartbeat) {
+          return
+        }
+        this.socketHeartbeat = setInterval(() => {
+          this.socket.send(JSON.stringify({
+            op: 3
+          }))
         }, 30000)
       } else if (data.op === 0) {
         this.user = data.d[uuid]
       }
     })
+
+    this.getTotalEther()
+    this.etherTimer = setInterval(() => {
+      this.getTotalEther()
+    }, 30000)
   },
   beforeDestroy () {
+    clearInterval(this.etherTimer)
     clearInterval(this.timer)
     this.socket?.close()
+  },
+  methods: {
+    getTotalEther () {
+      this.$axios.$get('https://api.eternal.gs/economy/stats')
+        .then((data) => {
+          this.totalEther = parseInt(data?.ether?.total)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
