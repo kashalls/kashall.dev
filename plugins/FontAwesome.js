148300a5310d603ba2c2001685df9f284e6ad162
Kashalls <kashalls@pm.me>
2022-10-30 11:50:36 -0700

    Add icons

--- plugins/FontAwesome.js
+++ plugins/FontAwesome.js
@@ -1,6 +1,6 @@
 /* Import all the font awesome goodness */
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-import { fas, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { fas, faMoon, faSackDollar, faCloud } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faDiscord, faSteam, faGithub, faInstagram, faReddit, faTwitch, faSpotify, faPatreon } from '@fortawesome/free-brands-svg-icons'
-library.add(fas, faTwitter, faDiscord, faSteam, faGithub, faInstagram, faReddit, faTwitch, faSpotify, faMoon, faPatreon)
+library.add(fas, faTwitter, faDiscord, faSteam, faGithub, faInstagram, faReddit, faTwitch, faSpotify, faMoon, faPatreon, faSackDollar, faCloud)
 dom.watch()
