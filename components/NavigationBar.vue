4ae7805adfff9fa83ec1acf7b8d2fad6884d66f6
Kashalls <kashalls@pm.me>
2022-10-30 11:50:27 -0700

    Add icons / condense to dropdowns

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -2,10 +2,7 @@
   <b-navbar transparent wrapper-class="container">
     <template #brand>
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <b-image
-          :src="require('@/assets/img/jordanjones-colored.svg')"
-          alt="Logo"
-        />
+        <b-image :src="require('@/assets/img/jordanjones-colored.svg')" alt="Logo" />
       </b-navbar-item>
     </template>
 
@@ -23,21 +20,20 @@
       </b-navbar-dropdown>
       <b-navbar-dropdown class="has-text-white" label="Affilates" hoverable>
         <b-navbar-item href="https://p.hyper.expert/aff.php?aff=99">
-          Hyper Expert
+          <i class="fas fa-cloud" /> Hyper Expert
         </b-navbar-item>
       </b-navbar-dropdown>
-      <b-navbar-item href="https://jordanjones.org/discord">
-        <i class="fab fa-discord" /> <span class="pl-1">Discord</span>
-      </b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: '/eternity' }">
-        <span class="pl-1">Eternity Balance</span>
-      </b-navbar-item>
-      <b-navbar-item href="https://debugmc.info">
-        <i class="fas fa-cubes" /> <span class="pl-1">DebugMC</span>
-        <b-tag rounded class="ml-1" type="is-info" size="is-small">
-          Beta
-        </b-tag>
-      </b-navbar-item>
+      <b-navbar-dropdown class="has-text-white" label="Projects" hoverable>
+        <b-navbar-item tag="router-link" :to="{ path: '/eternity' }">
+          <i class="fas fa-sack-dollar" /><span class="pl-1">Eternity</span>
+        </b-navbar-item>
+        <b-navbar-item href="https://debugmc.info">
+          <i class="fas fa-cubes" /> <span class="pl-1">DebugMC</span>
+          <b-tag rounded class="ml-1" type="is-info" size="is-small">
+            Beta
+          </b-tag>
+        </b-navbar-item>
+      </b-navbar-dropdown>
     </template>
   </b-navbar>
 </template>
