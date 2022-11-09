b464de6dfcea9ba2a5576824751c7c02b0436d18
Kashalls <kashalls@pm.me>
2022-11-09 13:44:29 -0800

    Color themeing

--- pages/index.vue
+++ pages/index.vue
@@ -3,22 +3,22 @@
     <div class="columns is-mobile is-variable is-8 is-flex-touch is-flex-direction-row-tablet is-vcentered">
       <div class="column">
         <div class="is-flex is-flex-direction-row is-align-items-center">
-          <h1 class="title is-1 has-text-white is-inline-block">
+          <h1 class="title is-1 is-inline-block">
             Kashall
           </h1>
-          <StatusIcon :status="status" :lanyard="lanyard" />
+          <StatusIcon :status="lanyard.discord_status" :lanyard="lanyard" />
         </div>
         <h2 class="subtitle is-3 has-text-white">
           <span class="wave mr-2">👋</span>Hey there! I'm Jordan.
         </h2>
-        <p class="has-text-white my-4">
+        <p class="my-4">
           I'm a <span class="has-text-primary">~{{ precisionAgeMs }}</span> year old self-taught web developer in the United States.
         </p>
-        <p class="has-text-white my-4">
+        <p class="my-4">
           Whilst I focus my attention learning how to become a better <a href="https://www.w3schools.com/whatis/whatis_fullstack_js.asp?utm_source=kashall.dev" target="_blank" rel="noreferrer noopener" class="is-underlined has-text-link is-italic">full-stack</a> web developer,<br>
           I have experience with all kinds of different system administor tasks such as Enterprise Networking Management, Phone Systems, Active Directory.
         </p>
-        <p class="has-text-white my-4">
+        <p class="my-4">
           I am currently looking for a position in the <span class="has-text-primary">IT Help Desk</span> field. If you're interesting in hiring me, <a href="mailto:pm@kashall.dev">email me</a>.
         </p>
         <link-container />
@@ -33,24 +33,25 @@
         />
       </div>
     </div>
-    <status-mask />
+    <hr>
   </section>
 </template>
 
 <script>
 import LinkContainer from '~/components/LinkContainer.vue'
 import StatusIcon from '~/components/StatusIcon.vue'
+
+let timer = null
+let lanyardSocket = null
+
 export default {
   name: 'IndexPage',
   components: { StatusIcon, LinkContainer },
   data () {
     return {
       lanyard: { discord_status: 'offline', loaded: false },
-      lanyardSocket: null,
       userId: '201077739589992448',
-      status: 'offline',
-      precisionAgeMs: 0,
-      timer: null
+      precisionAgeMs: 0
     }
   },
   computed: {
@@ -58,34 +59,23 @@ export default {
       return this.lanyard?.discord_user?.avatar.startsWith('a_') ? 'gif' : 'png'
     }
   },
-  watch: {
-    'lanyard.discord_status': {
-      handler (val) {
-        if (!val) {
-          return
-        }
-        this.status = val
-      },
-      deep: true
-    }
-  },
   async mounted () {
-    this.timer = setInterval(() => {
+    timer = setInterval(() => {
       this.precisionAge()
     }, 1000)
 
-    this.lanyardSocket = await this.$lanyard({
+    lanyardSocket = await this.$lanyard({
       userId: this.userId,
       socket: true
     })
-    this.lanyardSocket.addEventListener('message', ({ data }) => {
+    lanyardSocket.addEventListener('message', ({ data }) => {
       const { d: status } = JSON.parse(data)
       this.lanyard = status
     })
   },
   beforeDestroy () {
-    clearInterval(this.timer)
-    this.lanyardSocket?.close()
+    clearInterval(timer)
+    lanyardSocket?.close()
   },
   methods: {
     precisionAge () {
