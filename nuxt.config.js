772df4feda45eeea84a7a374c49afd14301e1f1e
Kashalls <kashalls@pm.me>
2022-11-09 15:49:26 -0800

    Remove font-awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '@/plugins/Util',
-    '@/plugins/FontAwesome.js',
     { src: '@/plugins/VueLanyard', mode: 'client' }
   ],
 
