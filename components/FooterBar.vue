f00f7349bc612f5499d46c554320e45f43052f9e
Kashalls <kashalls@pm.me>
2022-11-09 13:45:16 -0800

    Remove moon easter egg

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -3,7 +3,7 @@
   <footer class="footer-bar">
     <div class="container m-auto is-flex is-flex-direction-row py-6 is-align-content-center is-justify-content-space-between">
       <div class="is-flex is-flex-direction-row left-container mx-6">
-        <span class="is-clickable" @click="moon"><i class="fa-solid fa-moon" /></span>
+        <span class="is-clickable"><i class="fa-solid fa-moon" /></span>
         <span class="mx-4 is-unselectable">•</span>
         <nuxt-link to="/" class="is-unselectable">
           kashall.dev
@@ -19,45 +19,6 @@
     </div>
   </footer>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      count: 0,
-      timer: null
-    }
-  },
-  methods: {
-    moon () {
-      if (this.timer) { return }
-      if (this.count === 0) {
-        this.$buefy.toast.open('Light mode could hurt your eyes.\n I don\'t recommend it.')
-      }
-      if (this.count === 1) {
-        this.$buefy.toast.open('I\'m only trying to help. I don\'t think light mode was ever the right choice.')
-      }
-      if (this.count === 3) {
-        this.$buefy.toast.open('Stop! You don\'t want this to happen!')
-      }
-      if (this.count === 5) {
-        this.$buefy.toast.open('Don\'t do this!')
-      }
-      if (this.count === 10) {
-        this.$buefy.toast.open('I\'ve had it with your button mashing! You deserve this!')
-        // eslint-disable-next-line no-return-assign
-        setTimeout(() => location = 'https://youtu.be/bpU6w-kUKNE?t=29', 2000)
-      }
-      this.handleTimerCount()
-    },
-    handleTimerCount () {
-      // eslint-disable-next-line no-return-assign
-      this.timer = setTimeout(() => this.timer = null, 2000)
-      this.count++
-    }
-  }
-}
-</script>
 
 <style scoped>
 .left-container {
